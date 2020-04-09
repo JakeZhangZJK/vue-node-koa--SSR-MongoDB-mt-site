c70b7289d1e6a8c7bc6e89f4456d518ebc8fc8e8
Chocolate1999 <2816778271@qq.com>
2020-04-09 09:37:29 +0800

    搜索界面设计

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -1,7 +1,62 @@
 <!--  -->
 <template>
-    <div class="topbar">
-        <h3>Hello Vue</h3>
+    <div class="search-panel">
+        <el-row class="m-header-searchbar">
+            <el-col :span="3" class="left">
+                <img src="//s0.meituan.net/bs/fe-web-meituan/fa5f0f0/img/logo.png" alt="美团">
+            </el-col>
+            <el-col :span="15" class="center">
+                <div class="wrapper">
+                    <el-input placeholder="搜索商家或地点"/>
+                    <button class="el-button el-button-primary"><i class="el-icon-search"/></button>
+                    <dl class="hotPlace" style="display:none">
+                        <dt>热门搜索</dt>
+                        <dd>火锅</dd>
+                        <dd>火锅</dd>
+                        <dd>火锅</dd>
+                        <dd>火锅</dd>
+                        <dd>火锅</dd>
+                    </dl>
+                    <dl class="searchList" style="display:none">
+                        <dd>小吃</dd>
+                        <dd>小吃</dd>
+                        <dd>小吃</dd>
+                        <dd>小吃</dd>
+                    </dl>
+                </div>
+                <p class="suggest">
+                    <a href="#">故宫博物院</a>
+                    <a href="#">故宫博物院</a>
+                    <a href="#">故宫博物院</a>
+                    <a href="#">故宫博物院</a>
+                    <a href="#">故宫博物院</a>
+                </p>
+                <ul class="nav">
+                    <li>
+                        <nuxt-link to="/" class="takeout">美团外卖</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/" class="movie">猫眼电影</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/" class="hotel">美团酒店</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/" class="apartment">民宿/公寓</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/" class="business">商家入驻</nuxt-link>
+                    </li>
+                </ul>
+            </el-col>
+            <el-col :span="6" class="right">
+                <ul class="security">
+                    <li><i class="refund"/><p class="txt">随时退</p></li>
+                    <li><i class="single"/><p class="txt">不满意免单</p></li>
+                    <li><i class="overdue"/><p class="txt">过期退</p></li>
+                </ul>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
