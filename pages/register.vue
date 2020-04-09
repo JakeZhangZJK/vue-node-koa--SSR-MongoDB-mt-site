de58078674cf1768b16d60441dfd482050c891de
Chocolate1999 <2816778271@qq.com>
2020-04-09 14:13:56 +0800

    新建注册登录分支，添加注册模板

---
+++ pages/register.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="page-register">
+        111
+    </div>
+</template>
+
+<script>
+export default {
+    layout:'blank'
+}
+
+</script>
+<style lang='scss'>
+    
+</style>
