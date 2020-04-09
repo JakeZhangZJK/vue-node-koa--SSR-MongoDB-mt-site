9508f6b0af3d10ac60b6a6432c950883ed54c010
Chocolate1999 <2816778271@qq.com>
2020-04-09 13:46:46 +0800

    添加index中 life和slider页面

---
+++ components/index/artistic.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="m-istyle">
+    <dl @mouseover="over">
+      <dt>有格调</dt>
+      <dd
+        :class="{active:kind==='all'}"
+        kind="all"
+        keyword="景点">全部</dd>
+      <dd
+        :class="{active:kind==='part'}"
+        kind="part"
+        keyword="美食">约会聚餐</dd>
+      <dd
+        :class="{active:kind==='spa'}"
+        kind="spa"
+        keyword="丽人">丽人SPA</dd>
+      <dd
+        :class="{active:kind==='movie'}"
+        kind="movie"
+        keyword="电影">电影演出</dd>
+      <dd
+        :class="{active:kind==='travel'}"
+        kind="travel"
+        keyword="旅游">品质出游</dd>
+    </dl>
+    <ul class="ibody">
+      <li
+        v-for="item in cur"
+        :key="item.title">
+        <el-card
+          :body-style="{ padding: '0px' }"
+          shadow="never">
+          <img
+            :src="item.img"
+            class="image">
+          <ul class="cbody">
+            <li class="title">{{ item.title }}</li>
+            <li class="pos"><span>{{ item.pos }}</span></li>
+            <li class="price">￥<em>{{ item.price }}</em><span>/起</span></li>
+          </ul>
+        </el-card>
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss">
+    @import "@/assets/css/index/artistic.scss";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,19 +4,25 @@
       <el-col :span="5">
         <emenu/>
       </el-col>
-      <el-col :span="19">2</el-col>
+      <el-col :span="19">
+        <life/>
+      </el-col>
     </el-row>
     <el-row :span="24">
-        3 
+        <artistic/>
     </el-row>
   </div>
 </template>
 
 <script>
 import Emenu from '@/components/index/menu.vue'
+import Life from '@/components/index/life.vue'
+import Artistic from '@/components/index/artistic.vue'
 export default {
   components: {
-    Emenu
+    Emenu,
+    Life,
+    Artistic
   }
 }
 </script>
