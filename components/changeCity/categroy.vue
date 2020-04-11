7e21d36bbce452eb5dfa812216fbc6d529139694
Chocolate1999 <2816778271@qq.com>
2020-04-11 20:40:05 +0800

    城市搜索模块完成

--- components/changeCity/categroy.vue
+++ components/changeCity/categroy.vue
@@ -38,8 +38,10 @@ export default {
     if(status===200){
       let p
       let c
+      //获取首字母下的所有城市名
       let d={}
       city.forEach(item=>{
+        //获取拼音全拼，并取得首字母
         p=pyjs.getFullChars(item.name).toLocaleLowerCase().slice(0,1)
         c=p.charCodeAt(0)
         if(c>96&&c<123){
@@ -49,12 +51,14 @@ export default {
           d[p].push(item.name)
         }
       })
+      //从临时对象到数组的转变
       for(let [k,v] of Object.entries(d)){
         blocks.push({
           title:k.toUpperCase(),
           city:v
         })
       }
+      //根据首字母排序
       blocks.sort((a,b)=>a.title.charCodeAt(0)-b.title.charCodeAt(0))
       self.block=blocks
     }
--- pages/changeCity.vue
+++ pages/changeCity.vue
@@ -21,12 +21,12 @@
 <script>
 import iSelect from '@/components/changeCity/iselect.vue'
 import Hot from '@/components/changeCity/hot.vue'
-//import Categroy from '@/components/changeCity/categroy.vue'
+import Categroy from '@/components/changeCity/categroy.vue'
 export default {
   components:{
     iSelect,
     Hot,
-    //Categroy
+    Categroy
   }
 }
 </script>
