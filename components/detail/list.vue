7ccef3d68e8bf77d5263822afedcfbbdce519692
Chocolate1999 <2816778271@qq.com>
2020-04-16 09:26:11 +0800

    添加detail分支下list组件

---
+++ components/detail/list.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="m-detail-list">
+        <ul>
+            <li>{{list.filter(item=>item.photos.length).length}}款套餐</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        list:{
+            type:Array,
+            default:()=>{
+                return 
+            }
+        }
+    }
+}
+
+</script>
+<style lang='scss'>
+    
+</style>
