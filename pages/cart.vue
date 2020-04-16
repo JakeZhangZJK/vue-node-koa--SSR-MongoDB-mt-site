ba341d9226d828ac6e0f4924569902ddf5e65927
Chocolate1999 <2816778271@qq.com>
2020-04-16 10:32:23 +0800

    完成购物车静态模块搭建

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-        <h3>Hello Vue</h3>
-    </div>
+  <div class="page-cart">
+    <el-row>
+      <el-col
+        v-if="1 ||cart.length"
+        :span="24"
+        class="m-cart">
+        <list/>
+      </el-col>
+      <el-col
+        v-else
+        class="empty">购物车为空</el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import List from '@/components/cart/list.vue'
 export default {
-
+  components:{
+    List
+  },
+  data(){
+    return {
+      cart:[]
+    }
+  }
 }
-
 </script>
-<style lang='scss'>
-    
+
+<style lang="scss">
+  @import "@/assets/css/cart/index.scss";
 </style>
