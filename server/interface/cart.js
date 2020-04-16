6999c1095c92829940ff67e5e3c906fb258f4c8b
Chocolate1999 <2816778271@qq.com>
2020-04-16 11:02:03 +0800

    完成购物车api实现，添加注释，使用本地数据库

--- server/interface/cart.js
+++ server/interface/cart.js
@@ -1,11 +1,12 @@
 import Router from 'koa-router';
-import axios from './utils/axios'
 import Cart from '../dbs/models/cart'
 import md5 from 'crypto-js/md5'
 
 let router = new Router({prefix: '/cart'})
 
+//创建购物车和返回购物车相关信息
 router.post('/create', async ctx => {
+  //创建之前需要进行登录拦截
   if (!ctx.isAuthenticated()) {
     ctx.body = {
       code: -1,
@@ -20,13 +21,14 @@ router.post('/create', async ctx => {
         detail
       }
     } = ctx.request.body
+    //session下有个passport对象，里面存有user
     let cart = new Cart({id, cartNo, time, user: ctx.session.passport.user, detail})
     let result = await cart.save()
     if (result) {
       ctx.body = {
         code: 0,
         msg: '',
-        id: cartNo
+        id: cartNo  //返回购物车id
       }
     } else {
       ctx.body = {
@@ -36,10 +38,10 @@ router.post('/create', async ctx => {
     }
   }
 })
-
+//获取购物车信息
 router.post('/getCart', async ctx => {
   let {id} = ctx.request.body
-  console.log(id);
+  //console.log(id);
   try {
     let result = await Cart.findOne({cartNo: id})
     ctx.body = {
